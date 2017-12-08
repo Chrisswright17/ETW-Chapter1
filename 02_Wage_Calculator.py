30b81e4458509a8f3d7cf5af66d7dab83ea80d06
Chrisswright17 <34344173+Chrisswright17@users.noreply.github.com>
2017-12-08 13:44:33 -0500

    Updated the wage calculator
    I changed the  equation in line 4 to have a float conversion for the hours variable

--- 02_Wage_Calculator.py
+++ 02_Wage_Calculator.py
@@ -1,6 +1,6 @@
 #In this program im trying to make a calculator that will calculate your pay based on hours worked#
 min_wage = 8.15
 hours = input("How many hours will you work this week? ") 
-pay = min_wage * hours
+pay = min_wage * float(hours)
 
 print(pay)
