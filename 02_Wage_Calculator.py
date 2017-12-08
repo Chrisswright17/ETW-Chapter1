369b40dbd8e650c99a9e4dbe0ddcaa0cc32c8238
Christofer Scott Wright <CWright1220@euclid.k12.oh.us>
2017-12-08 13:10:13 -0500

    Added a Wage Calculator program

---
+++ 02_Wage_Calculator.py
@@ -0,0 +1,6 @@
+#In this program im trying to make a calculator that will calculate your pay based on hours worked#
+min_wage = 8.15
+hours = input("How many hours will you work this week? ") 
+pay = min_wage * hours
+
+print(pay)
